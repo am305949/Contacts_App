c18e4d3ea96480320d02ef1b3745330e4851898e
Ahmed.M.Ibrahim <35659429+am305949@users.noreply.github.com>
2021-09-25 13:39:34 +0200

    Add files via upload

---
+++ ContactsApp/wwwroot/js/keypad.js
@@ -0,0 +1,15 @@
+﻿function addNumber(number) {
+    var num = document.forms[0].Number;
+    num.value += number;
+}
+
+function removeNumber() {
+    var num = document.forms[0].Number.value;
+    var last_char = num.charAt(num.length - 1);
+    var new_num = num.slice(0, num.length - 1);
+    document.forms[0].Number.value = new_num;
+}
+
+function emptyNumber() {
+    document.forms[0].Number.value = "";
+}
